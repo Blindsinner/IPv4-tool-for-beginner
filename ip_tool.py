26e3d7fa3634eae2c72f005737f2029ed4f26733
theLazyProgrammer <nanaforsonjnr@gmail.com>
2023-04-08 13:40:06 +0000

    Improved the function to retrieve subnet information, currently number 5. Also added a function to handle the presentation

--- ip_tool.py
+++ ip_tool.py
@@ -47,7 +47,8 @@ def ip_to_binary(ip_address):
     else:
         # Pad with zeros if necessary
         binary_ip = binary_ip.rjust(ip_bits_total, '0')
-        return successful(f'Binary IP Address: {".".join([binary_ip[i:i+ip_octet] for i in range(0, ip_bits_total, ip_octet)])}')
+        return successful(
+            f'Binary IP Address: {".".join([binary_ip[i:i+ip_octet] for i in range(0, ip_bits_total, ip_octet)])}')
 
 
 def network_address(ip_address, subnet_mask):
@@ -67,27 +68,102 @@ def cidr_to_subnet_mask(ip_address):
     except ipaddress.AddressValueError:
         print(error("Error: Invalid subnet mask or IP address"))
     else:
-        return str(successful(f'IP Subnet: {network.network_address}\nIP Subnet Mask: {network.netmask}'))
+        return str(successful(f'Network Address: {network.network_address}\nSubnet Mask: {network.netmask}'))
 
 
-def possible_number_of_subnets(ip_address, subnet_mask):
+def ip_range_info(ip_network, subnet_mask, new_prefix=None):
     try:
-        ip = ipaddress.IPv4Network(f'{ip_address}/{subnet_mask}', strict=False)
+        ip = ipaddress.IPv4Network(f'{ip_network}/{subnet_mask}', strict=False)
     except ipaddress.AddressValueError:
         print(error("Error: Invalid subnet mask or IP address"))
-    else:    
+    else:
         # get the number of bits left in the host portion
-        return successful(f'Possible Number of Subnets: {2 ** (ip_bits_total - ip.prefixlen)}')
+        total_usable_ip_addr = [addr for addr in ip.hosts()]
+        first_usable_ip = total_usable_ip_addr[0]
+        last_usable_ip = total_usable_ip_addr[-1]
+
+        # selectively data if user chose to get the subnets and not just default info
+        if new_prefix:
+            # first subnet.
+            first_usable_subnet = get_subnets(
+                ip_network, subnet_mask, new_prefix)[0]
+            # last subnet
+            last_usable_subnet = get_subnets(
+                ip_network, subnet_mask, new_prefix)[-1]
+            # total number of subnets
+            total_possible_subnets = len(get_subnets(
+                ip_network, subnet_mask, new_prefix))
+            # checks total assignable host addresses
+            total_assignable_host_addresses = ipaddress.ip_network(get_subnets(
+                ip_network, subnet_mask, new_prefix)[0]).num_addresses
+            
+            return {
+                f'Total Subnets Possible with prefix /{new_prefix}': total_possible_subnets,
+               'Total assignable IP Addresses per subnet': total_assignable_host_addresses,
+                'First usable subnet': first_usable_subnet,
+                'Last usable subnet': last_usable_subnet,
+            }
+
+        # default info
+        return {
+            'Possible Number of Subnets': f'{2 ** (ip_bits_total - ip.prefixlen)}',
+            'Total Assignable IP Address': len(total_usable_ip_addr),
+            'First assignable IP Address': first_usable_ip,
+            'Last assignable IP Address': last_usable_ip,
+            'Broadcast Address': ip.broadcast_address,
+        }
+
+
+def print_subnets(network_info):
+    try:
+        for title, data in network_info.items():
+            print(successful(f'{title}: {data}'))
+
+    except:
+        print(error("Invalid network info"))
+
+
+def get_subnets(ip_network, subnet_mask, prefix=None):
+    """Takes care of spitting out the possible subnets that can be created
+
+    ip_network: The base IP network/subnet address (E.g. 192.168.10.0)
+    subnet_mask: Accepts either DDN subnet mask or CIDR (E.g. 255.255.255.0 or 24)
+    prefix (optoinal): The new prefix/subnet mask to subnet from 
+                       the base network/subnet address. (E.g. 27)
+
+    Examples: 
+        - get_subnets("192.168.10.0", "255.255.255.0", 27)
+        - get_subnets("192.168.10.0", 24, 27)
+        - get_subnets("192.168.10.0", 24)
+    """
+    if prefix:
+        network = ipaddress.IPv4Network(
+            f"{ip_network}/{subnet_mask}", strict=False)
+        try:
+            subnets = []
+            for subnet in network.subnets(new_prefix=int(prefix)):
+                subnets.append(str(subnet))
+            return subnets
+        except ValueError as p_error:
+            print(error(p_error))
+
+    else:
+        return ipaddress.IPv4Network(
+            f"{ip_network}/{subnet_mask}", strict=False)
 
 
 def ip_class_private_public(ip_address):
-    """Is it a Private Address or Public Address? That's what this function answers"""
+    """Is it a Private Address or Public Address? That's what this function answers
+    This function is limited to using the first octet to determine the classful address.
+    
+    It is possible to 192.168.0.0/20 and still receive a Class C because of the first octet.
+    """
     try:
         # confirm validity of address received
         ip = ipaddress.IPv4Address(ip_address)
     except ipaddress.AddressValueError:
         print(error("Error: Invalid Addresss"))
-    else:    
+    else:
         first_octet = int(str(ip).split('.')[0])
         # class A -> 1 - 126
         if first_octet >= 1 and first_octet <= 126:
@@ -101,17 +177,17 @@ def ip_class_private_public(ip_address):
         # class D -> 224 - 239
         elif first_octet >= 224 and first_octet <= 239:
             ip_class = 'D'
-        # class E -> 240 - 255 
+        # class E -> 240 - 255
         else:
             ip_class = 'E'
-    
-        # return results    
+
+        # return results
         if ip.is_private:
             return f"{successful(f'IP class and private/public: Class {ip_class}, Private')}"
         else:
             return f"{successful(f'IP class and private/public: Class {ip_class}, Public')}"
-        
-    
+
+
 def display_all_info(ip_address, subnet_mask_or_cidr):
     """A consolidate view for IP Address info"""
     try:
@@ -126,9 +202,9 @@ def display_all_info(ip_address, subnet_mask_or_cidr):
             f'{ip_address}/{subnet_mask}', strict=False).prefixlen
         print(successful(f"IP Address: {ip_address}"))
         print(successful(f"Subnet Mask: {subnet_mask}"))
-    finally:    
+    finally:
         print(f"{successful(f'CIDR Notation: {cidr}')}")
-        print(possible_number_of_subnets(ip_address, cidr))
+        print_subnets(ip_range_info(ip_address, cidr))
         print(network_address(ip_address, cidr))
         print(ip_class_private_public(ip_address))
         print(ip_to_binary(ip_address))
@@ -185,7 +261,9 @@ def run_tool():
                         # possible number of subnets
                         ip_address = input("Enter the IP network: ")
                         subnet_mask = input("Enter the subnet mask or CIDR: ")
-                        print(possible_number_of_subnets(ip_address, subnet_mask))
+                        new_prefix = input("Enter new netmask/CIDR: ")
+                        print_subnets(ip_range_info(
+                            ip_address, subnet_mask, new_prefix))
 
                     case 6:
                         # is it a private or public IP address?
@@ -195,7 +273,8 @@ def run_tool():
                     case 7:
                         # get a nice consolidated view on the IP address
                         ip_address = input("Enter the IP address: ")
-                        subnet_mask_or_cidr = input("Enter the subnet mask or CIDR notation: ")
+                        subnet_mask_or_cidr = input(
+                            "Enter the subnet mask or CIDR notation: ")
                         display_all_info(ip_address, subnet_mask_or_cidr)
 
                     case 8:
@@ -205,10 +284,14 @@ def run_tool():
                     case _:
                         # input validation
                         print(error('Invalid option. Please try again.'))
-            
+
             except (KeyboardInterrupt, EOFError):
                 sys.exit(error("Exited"))
 
 
 if __name__ == "__main__":
     run_tool()
+
+
+# TODO: Make functios to return only data, this will allow for easier testing
+# TODO: Use functions to format and present data. Have a function for getting data and another for presenting
